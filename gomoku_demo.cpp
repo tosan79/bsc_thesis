98d8470f8ce237e0d817b090d31b42a6e5bbac21
tosan79 <atakamona@gmail.com>
2024-08-17 23:42:15 +0200

    some repo reorganizations

--- gomoku_demo.cpp
+++ gomoku_demo.cpp
@@ -11,7 +11,6 @@ public:
     //unsigned int current_player; // either 0 ('O') or 1 ('X')
 
     Board() {
-        //current_player = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 board[i][j] = '.';
@@ -56,43 +55,43 @@ public:
     void log_move() {
         std::cout << "[" << last_move[0] << ", " << last_move[1] << "], " << std::endl;
     }
+
+    char switch_player(char player) {
+        return player == 'O' ? 'X' : 'O';
+    }
 };
 
+// make_random_move() function could be kind of an interface
+// for a person writing a bot to communicate with the game itself
 void make_random_move(Board &B, char player) {
     std::srand(std::time(nullptr));
-    int x = std::rand() % N;
-    int y = std::rand() % N;
+    
+    int x, y;
+    do {
+        x = std::rand() % N;
+        y = std::rand() % N;   
+    } while (B.board[x][y] != '.');
 
-    while (B.board[x][y] != '.') {
-        x = rand() % N;
-        y = rand() % N;
-    }
-
-    // Make the move
     B.make_move(x, y, player);
     B.last_move[0] = x;
     B.last_move[1] = y;
 }
 
 int main() {
-    Board B;
-    // B.print();
     std::cout << "[";
+    
+    Board B;
 
     while (true) {
         make_random_move(B, 'O'); // 'O' starts always
         if (B.checkWin(B.last_move[0], B.last_move[1], 'O')) {
-            // std::cout << "O wins!" << std::endl;
             std::cout << "[" << B.last_move[0] << ", " << B.last_move[1] << "]";
             break;
         }
         B.log_move();
 
         make_random_move(B, 'X');
-        // B.print();
-        // std::cout << "(last move: " << B.last_move[0] << ", " << B.last_move[1] << ")" << std::endl;
         if (B.checkWin(B.last_move[0], B.last_move[1], 'X')) {
-            // std::cout << "X wins!" << std::endl;
             std::cout << "[" << B.last_move[0] << ", " << B.last_move[1] << "]";
             break;
         }
